e62ebc8cfb9c97aba4149af2839ddcf61dd1cdd3
Jeremy <yiliang.he@qq.com>
2025-04-06 11:46:15 +0800

    Update chain config

--- types/v0/v0_3/chain_config.go
+++ types/v0/v0_3/chain_config.go
@@ -10,12 +10,12 @@ import (
 )
 
 type ChainConfig struct {
-	ChainId      common_types.U256Decimal `json:"chain_id"`
-	MaxBlockSize common_types.U256Decimal `json:"max_block_size"`
-	BaseFee      common_types.U256Decimal `json:"base_fee"`
-	FeeContract  *common.Address          `json:"fee_contract"`
-	FeeRecipient common.Address           `json:"fee_recipient"`
-	BidRecipient *common.Address          `json:"bid_recipient"`
+	ChainId            common_types.U256Decimal `json:"chain_id"`
+	MaxBlockSize       common_types.U256Decimal `json:"max_block_size"`
+	BaseFee            common_types.U256Decimal `json:"base_fee"`
+	FeeContract        *common.Address          `json:"fee_contract"`
+	FeeRecipient       common.Address           `json:"fee_recipient"`
+	StakeTableContract *common.Address          `json:"stake_table_contract"`
 }
 
 func (self *ChainConfig) Commit() common_types.Commitment {
@@ -30,8 +30,8 @@ func (self *ChainConfig) Commit() common_types.Commitment {
 		builder.Uint64Field("fee_contract", 0)
 	}
 
-	if self.BidRecipient != nil {
-		builder.FixedSizeField("bid_recipient", self.BidRecipient.Bytes())
+	if self.StakeTableContract != nil {
+		builder.FixedSizeField("stake_table_contract", self.StakeTableContract.Bytes())
 	}
 	return builder.Finalize()
 }
