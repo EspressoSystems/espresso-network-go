3202243f8e4ce459ebe7d584b6b05c094e3d272f
Sneh Koul <snehkoul1999@gmail.com>
2025-04-30 08:12:24 +0530

    generate verification data

--- verification/native_test.go
+++ verification/native_test.go
@@ -48,43 +48,43 @@ func TestMerkleProofVerification(t *testing.T) {
 
 }
 
-// type namespaceProofTestData struct {
-// 	NsProof   json.RawMessage `json:"ns_proof"`
-// 	VidCommit string          `json:"vid_commit"`
-// 	VidCommon json.RawMessage `json:"vid_common"`
-// 	Namespace uint64          `json:"namespace"`
-// 	NsTable   []uint8         `json:"ns_table"`
-// 	TxCommit  string          `json:"tx_commit"`
-// }
+type namespaceProofTestData struct {
+	NsProof   json.RawMessage `json:"ns_proof"`
+	VidCommit string          `json:"vid_commit"`
+	VidCommon json.RawMessage `json:"vid_common"`
+	Namespace uint64          `json:"namespace"`
+	NsTable   []uint8         `json:"ns_table"`
+	TxCommit  string          `json:"tx_commit"`
+}
 
-// func TestNamespaceProofVerification(t *testing.T) {
-// 	file, err := os.Open("./namespace_proof_test_data.json")
-// 	if err != nil {
-// 		log.Fatalf("Failed to open file: %v", err)
-// 	}
-// 	defer file.Close()
+func TestNamespaceProofVerification(t *testing.T) {
+	file, err := os.Open("./namespace_proof_test_data.json")
+	if err != nil {
+		log.Fatalf("Failed to open file: %v", err)
+	}
+	defer file.Close()
 
-// 	bytes, err := io.ReadAll(file)
-// 	if err != nil {
-// 		log.Fatalf("Failed to read file")
-// 	}
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("Failed to read file")
+	}
 
-// 	var data namespaceProofTestData
+	var data namespaceProofTestData
 
-// 	if err := json.Unmarshal(bytes, &data); err != nil {
-// 		log.Fatalf("Failed to unmarshal the test data: %v", err)
-// 	}
+	if err := json.Unmarshal(bytes, &data); err != nil {
+		log.Fatalf("Failed to unmarshal the test data: %v", err)
+	}
 
-// 	r := verifyNamespace(data.Namespace, data.NsProof, []byte(data.VidCommit), data.NsTable, []byte(data.TxCommit), data.VidCommon)
-// 	if !r {
-// 		log.Fatalf("Failed to verify the namespace proof")
-// 	}
+	r := verifyNamespace(data.Namespace, data.NsProof, []byte(data.VidCommit), data.NsTable, []byte(data.TxCommit), data.VidCommon)
+	if !r {
+		log.Fatalf("Failed to verify the namespace proof")
+	}
 
-// 	// Tamper with the correct data and see if it will return false
-// 	data.Namespace = 1
+	// Tamper with the correct data and see if it will return false
+	data.Namespace = 1
 
-// 	r = verifyNamespace(data.Namespace, data.NsProof, []byte(data.VidCommit), data.NsTable, []byte(data.TxCommit), data.VidCommon)
-// 	if r {
-// 		log.Fatalf("Failed to verify the namespace proof")
-// 	}
-// }
+	r = verifyNamespace(data.Namespace, data.NsProof, []byte(data.VidCommit), data.NsTable, []byte(data.TxCommit), data.VidCommon)
+	if r {
+		log.Fatalf("Failed to verify the namespace proof")
+	}
+}
